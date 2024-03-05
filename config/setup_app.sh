ef1b837426aaa9e488f8ece6f34c2e67bf18816d
Olivier Leal <olivierleal61@gmail.com>
2024-03-05 08:35:07 -0300

    Commit Final

--- config/setup_app.sh
+++ config/setup_app.sh
@@ -8,12 +8,4 @@ if [ -f tmp/pids/server.pid ]; then
     rm tmp/pids/server.pid
 fi
 
-if ["$RAILS_ENV" != development]; then
-    echo '--> Running migrations'
-    rails db:migrate
-    echo '--> End of Migrations'
-else
-    echo '--> Skip migraions for dev env'
-fi
-
 exec "$@"
