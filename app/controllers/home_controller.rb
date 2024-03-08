93a4c0e393a894334124b846a7b6446cc5afbd99
Olivier Leal <101745595+OlivierLeal@users.noreply.github.com>
2024-03-08 10:03:01 -0300

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationControll:error
     def index
-        (1..5).to_a.each do |number|
-            sleep 1
-            SendEmailJob.perform_later
-        end
         @message = 'Carregado'
     end
 end
